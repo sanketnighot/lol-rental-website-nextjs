cdf0b0bb25d39b4204cd8f723bbb939886dcc26c
sanketnighot <sanketnighot25@gmail.com>
2022-12-26 20:03:12 +0530

    contract address update land

--- Components/HomePage.jsx
+++ Components/HomePage.jsx
@@ -27,7 +27,7 @@ const Item = styled(Paper)(({ theme }) => ({
     color: theme.palette.text.secondary,
   }));
 
-export const landContractAddress = "0x31ff0f9638b04d34dd5fa0f2521cefa86f83bd26";
+export const landContractAddress = "0x339f39f2c458fb9b0053e3116e00b1f2b607ba31";
 export const lordContractAddress = "0x163ccc9719139c2e0b7543738e7f0de67bba75d5";
 export const rentalContractAddress = "0xca63b89db5a634ad465927ff63e0fd1495928e23";
 
